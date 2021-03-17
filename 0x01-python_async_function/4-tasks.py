c8ec486c13d7d283552e669b9bfe81bbbaee4921
zacwoll <zacwoll@gmail.com>
2021-03-16 17:45:18 -0700

    added type hinting

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,7 +5,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Generates asyncio.Task list of delays """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = [await task for task in asyncio.as_completed(tasks)]
