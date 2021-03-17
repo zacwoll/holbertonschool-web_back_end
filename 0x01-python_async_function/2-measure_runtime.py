5e9d3d8e7442f71b37a4fc45d959cc2c2914f208
zacwoll <zacwoll@gmail.com>
2021-03-16 17:40:01 -0700

    set up module imports before local imports

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Measure time of function """
-wait_n = __import__('1-concurrent_coroutines').wait_n
 import asyncio
 import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Create an asyncio Task """
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
