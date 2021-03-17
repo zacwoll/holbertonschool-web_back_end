8be75bcbb740a9f7e1b39aa4f9474a37ee3c9d54
zacwoll <zacwoll@gmail.com>
2021-03-16 17:48:33 -0700

    removed unnecessary whitespace

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay : int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ Awaits random delay between 0 & max-delay """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
