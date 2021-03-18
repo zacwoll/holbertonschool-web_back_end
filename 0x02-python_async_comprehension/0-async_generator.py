d7c85775d351b7de9d00be55f7406eabc4e0ebd9
zacwoll <zacwoll@gmail.com>
2021-03-18 10:55:55 -0700

    Fixed the type annotation typo

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, NoneType, NoneType]:
+async def async_generator() -> Generator[float, None, None]:
     """ Generates random numbers [0.0, 10.0) and sleeps for 1 second """
     for _ in range(10):
         await asyncio.sleep(1)
