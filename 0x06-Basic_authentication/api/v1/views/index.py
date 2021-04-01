32c80d40dbac608a5363e767314e19aa0c352ec8
zacwoll <zacwoll@gmail.com>
2021-04-01 12:09:40 -0700

    fixed bug in route name, added no strict_slashes flag

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -35,7 +35,7 @@ def unauthorized() -> str:
     abort(401)
 
 
-@app_views.route('/Forbidden')
+@app_views.route('/forbidden', strict_slashes=False)
 def forbidden() -> str:
     """ GET /api/v1/forbidden
     Return:
