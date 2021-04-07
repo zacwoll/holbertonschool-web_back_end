f79c920eeec2565a8bcde4177d2ca50be306bd83
zacwoll <zacwoll@gmail.com>
2021-04-07 16:36:57 -0700

    Edited to check for failures not success to return None, abort(401)

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -69,7 +69,8 @@ def before():
     if auth.current_user(request) is None:
         abort(403)
 
-    if auth.authorization_header(request) and auth.session_cookie(request):
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         return None, abort(401)
 
     request.current_user = auth.current_user(request)
