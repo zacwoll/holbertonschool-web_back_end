5c50a6cd427665e3f4a020e93c0b3d6afb6c962c
zacwoll <zacwoll@gmail.com>
2021-04-07 19:13:13 -0700

    modified authorization to allow either header or cookie

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -63,15 +63,12 @@ def before():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
         abort(403)
-
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
-        return None, abort(401)
 
     request.current_user = auth.current_user(request)
 
