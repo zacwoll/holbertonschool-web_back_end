9ceeae2a0815da5c9d0cdd7d4c3efd63fc1e5f46
zacwoll <zacwoll@gmail.com>
2021-04-12 13:19:31 -0700

    Added Integrity Error, collapsed user imports

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,10 +4,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import IntegrityError
 from typing import TypeVar
 
-from user import Base
-from user import User
+from user import Base, User
 
 
 class DB:
