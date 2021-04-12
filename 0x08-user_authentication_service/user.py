f1c943323fa6698aaedd1a3aeb2fbdb52729b573
zacwoll <zacwoll@gmail.com>
2021-04-12 13:16:03 -0700

    Removed init function

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -16,7 +16,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __init__(self, email, hashed_password):
-        self.email = email
-        self.hashed_password = hashed_password
