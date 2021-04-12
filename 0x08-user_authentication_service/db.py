3156abc27b4ee3fa3afaf8a9b745f98a8384488f
zacwoll <zacwoll@gmail.com>
2021-04-12 13:22:18 -0700

    User Creation needs kwargs instead (removed explicit init)

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -25,9 +25,9 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str):  # -> TypeVar(User):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds a user """
-        new_user = User(email, hashed_password)
+        new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
