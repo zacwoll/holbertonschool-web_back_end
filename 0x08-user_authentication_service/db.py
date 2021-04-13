580e07aa4c80507a6eda25564b120d3444539273
zacwoll <zacwoll@gmail.com>
2021-04-13 13:45:05 -0700

    cleaned up the imports

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -3,8 +3,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import IntegrityError
 
 from user import Base, User
 
@@ -33,7 +31,6 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """ Returns first row found in users table with args from kwargs """
-
         return self._session.query(User).filter_by(**kwargs).one()
 
     def update_user(self, user_id: int, **kwargs) -> None:
--- 0x08-user_authentication_service/main.py
+++ 0x08-user_authentication_service/main.py
@@ -2,13 +2,14 @@
 """
 Main file
 """
-from auth import Auth
 
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
-auth = Auth()
+from db import DB
+from user import User
 
-auth.register_user(email, password)
+my_db = DB()
 
-print(auth.create_session(email))
-print(auth.create_session("unknown@email.com"))
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
+
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
