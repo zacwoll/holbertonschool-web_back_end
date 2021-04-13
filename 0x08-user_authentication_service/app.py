4edef93651a83632abba3c470a0da531aa177494
zacwoll <zacwoll@gmail.com>
2021-04-13 16:49:00 -0700

    fixed typo

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -66,7 +66,7 @@ def profile():
         abort(403)
 
     user = AUTH.get_user_from_session_id(session_id)
-    if user is NOne:
+    if user is None:
         abort(403)
 
     return jsonify({"email": user.email}), 200
