e0e374ea23b4de18752c2b1456076d22f1668d28
zacwoll <zacwoll@gmail.com>
2021-04-13 16:46:29 -0700

    added strict_slashes

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -58,7 +58,7 @@ def logout():
     return redirect('/')
 
 
-@app.route('/profile')
+@app.route('/profile', methods=['GET'], strict_slashes=False)
 def profile():
     """ Returns a users profile """
     session_id = request.cookies.get("session_id")
@@ -85,7 +85,7 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": token}), 200
 
 
-@app.route('/reset_password', methods=["PUT"])
+@app.route('/reset_password', methods=["PUT"], strict_slashes=False)
 def update_password():
     """ Updates user password """
     email = request.form.get('email')
