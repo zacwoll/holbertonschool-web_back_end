a26f5c540cd4549b7344c20e11e0e89632e95723
zacwoll <zacwoll@gmail.com>
2021-04-21 16:43:24 -0700

    First couple files, pybabels not working

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Basic Flask App """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ Config class """
+    LANGUAGES = ['en', 'fr']
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config.from_object(Config)
+babel = Babel(app)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
+
+
+@app.route('/', methods=["GET"])
+def default_route():
+    """ Returns a greeting """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ 0x0A-i18n/README.md
@@ -0,0 +1,12 @@
+# 0x0A. i18n
+## Internationalization and localization
+
+### Resources
+Read or watch:
+- Flask-Babel : https://intranet.hbtn.io/rltoken/Q71CxQOjqpOJrqHd_F4lXQ
+- Flask i18n tutorial : https://intranet.hbtn.io/rltoken/NdAnX-Td57RRaA25LX0A1Q
+- pytz : https://intranet.hbtn.io/rltoken/yk8MxfbrtfmHusK6pmX7XQ
+### Learning Objectives
+- Learn how to parametrize Flask templates to display different languages
+- Learn how to infer the correct locale based on URL parameters, user settings or request headers
+- Learn how to localize timestamps
