cfa467d4065fffe002c48d11d47cad6307af499a
zacwoll <zacwoll@gmail.com>
2021-05-06 13:30:44 -0700

    reverted changes to what I believe is the answer

--- 0x0E-ES6_basic/0-constants.js
+++ 0x0E-ES6_basic/0-constants.js
@@ -4,12 +4,12 @@ export function taskFirst() {
 }
 
 export function getLast() {
-return ' is okay';
+  return ' is okay';
 }
 
 export function taskNext() {
-let combination = 'But sometimes let';
-combination += getLast();
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-return combination;
+  return combination;
 }
