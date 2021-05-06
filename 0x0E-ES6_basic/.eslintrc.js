f998e1b0112ff6504e30e075f4e66f3d3541daf0
zacwoll <zacwoll@gmail.com>
2021-05-06 13:34:36 -0700

    accidentally modified file

--- 0x0E-ES6_basic/.eslintrc.js
+++ 0x0E-ES6_basic/.eslintrc.js
@@ -1,33 +1,35 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true
+    env: {
+      browser: false,
+      es6: true,
+      jest: true,
     },
-    'extends': 'eslint:recommended',
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
     },
-    'parserOptions': {
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
     },
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+    plugins: ['jest'],
+    rules: {
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    overrides:[
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      }
+    ]
 };
