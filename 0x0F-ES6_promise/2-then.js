e7b314573638466819c9352987eeaa4dc93cb199
zacwoll <zacwoll@gmail.com>
2021-05-11 10:28:51 -0700

    fixed formatting

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,5 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error()).finally(() => {
-    console.log('Got a response from the API');
-  });
+  return promise.then(() => ({
+    status: 200,
+    body: 'success',
+  }))
+    .catch(() => Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
