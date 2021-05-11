fbd9babb68c8d075ca3a44fbe118f8f3a2f88d6f
zacwoll <zacwoll@gmail.com>
2021-05-11 10:24:56 -0700

    returned the object and incorporated the .catch

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,8 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'success',
-  }))
-    .catch(() => Error())
+  promise.then(() => {
+    return {
+      'status': 200,
+      'body': 'success'
+    };
+  }, () => {
+    return new Error();
+  })
     .finally(() => console.log('Got a response from the API'));
 }
