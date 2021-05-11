f550439b2fd671e0872a28a7adebbc97d4ab03e7
zacwoll <zacwoll@gmail.com>
2021-05-11 10:27:12 -0700

    replaced the .catch statement moved the return up

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,7 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'success',
-  }), () => new Error())
-    .finally(() => console.log('Got a response from the API'));
+  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error()).finally(() => {
+    console.log('Got a response from the API');
+  });
 }
