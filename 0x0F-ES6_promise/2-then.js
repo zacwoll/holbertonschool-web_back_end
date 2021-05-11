f826e9ad09e4155f462d1f3fbeae1b5ac5ac5d3a
zacwoll <zacwoll@gmail.com>
2021-05-11 10:25:30 -0700

    fixed line endings to linux

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,11 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    return {
-      'status': 200,
-      'body': 'success'
-    };
-  }, () => {
-    return new Error();
-  })
+  promise.then(() => ({
+    status: 200,
+    body: 'success',
+  }), () => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
