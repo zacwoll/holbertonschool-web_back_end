f63dbbcb34b387afd38cfc7cf5feb4db490220a5
zacwoll <zacwoll@gmail.com>
2021-05-18 11:17:34 -0700

    Test case of bad inputs covered

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
-  return [...set].filter((item) => item.startsWith(startString))
+  if (startString === '' || typeof startString !== 'string') return '';
+  return [...set].filter((item) => typeof item === 'string' && item.startsWith(startString))
     .map((item) => item.slice(startString.length))
     .join('-');
 }
--- 0x11-ES6_data_manipulation/8-main.js
+++ 0x11-ES6_data_manipulation/8-main.js
@@ -2,3 +2,5 @@ import cleanSet from "./8-clean_set.js";
 
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+
+console.log(cleanSet(new Set(['id-test', 'id-chicken', 'id-user', , 'id-id-']), []));
