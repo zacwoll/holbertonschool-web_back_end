fd752cda9715993ce0a4779be4539d2847564aaf
zacwoll <zacwoll@gmail.com>
2021-05-19 14:58:33 -0700

    First couple tasks

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+  return fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) throw Error('Cannot load the database');
+    const students = data.split('\r\n').slice(1)
+      .map((student) => student.split(','))
+      .map((student) => ({
+        firstName: student[0],
+        lastName: student[1],
+        age: student[2],
+        field: student[3],
+      }));
+    const csStudents = students.filter((student) => student.field === 'CS')
+      .map((student) => student.firstName);
+    const sweStudents = students.filter((student) => student.field === 'SWE')
+      .map((student) => student.firstName);
+    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+  });
+}
+
+module.exports = countStudents;
