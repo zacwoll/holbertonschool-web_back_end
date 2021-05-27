044945ac341e4ff2be8400ec16cd4766feb87396
zacwoll <zacwoll@gmail.com>
2021-05-26 21:28:15 -0700

    adjusted tests to make them clearer to checker

--- 0x13-unittests_in_js/9-api/api.test.js
+++ 0x13-unittests_in_js/9-api/api.test.js
@@ -1,42 +1,38 @@
 const { expect } = require("chai");
 const request = require('request');
+const url = 'http://localhost:7865';
 
 describe('Index page', () => {
-    describe('GET /', () => {
-        it('checks output of GET /', (done) => {
-            request('http://localhost:7865', (err, res, body) => {
-                expect(res.statusCode).to.equal(200);
-                expect(body).to.equal('Welcome to the payment system');
-            }, done());
+    it('GET / exists and outputs', (done) => {
+        request(url, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            expect(body).to.equal('Welcome to the payment system');
+            done();
         });
     });
 
-    describe('GET /cart/:id', () => {
-        it('checks output of GET /cart/:id', (done) => {
-            request('http://localhost:7865/cart/12', (err, res, body) => {
-                expect(res.statusCode).to.equal(200);
-                expect(body).to.equal('Payment methods for cart 12')
-            }, done());
+    it('GET /cart/:id', (done) => {
+        request(`${url}/cart/12`, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            expect(body).to.equal('Payment methods for cart 12');
+            done();
         });
     });
 
-    describe('GET /cart/:id with id hello', () => {
-        it('http://localhost:7865/cart/hello', (done) => {
-            request('http://localhost:7865/cart/hello', (err, res, body) => {
-                if (res) {
-                    expect(res.statusCode).to.equal(404);
-                    expect(body).to.equal('<!DOCTYPE html>\n' +
-                    '<html lang="en">\n' +
-                    '<head>\n' +
-                    '<meta charset="utf-8">\n' +
-                    '<title>Error</title>\n' +
-                    '</head>\n' +
-                    '<body>\n' +
-                    '<pre>Cannot GET /cart/hello</pre>\n' +
-                    '</body>\n' +
-                    '</html>\n');
-                }
-            }, done());
+    it('GET /cart/:id with id hello', (done) => {
+        request(`${url}/cart/hello`, (err, res, body) => {
+            expect(res.statusCode).to.equal(404);
+            expect(body).to.equal('<!DOCTYPE html>\n' +
+            '<html lang="en">\n' +
+            '<head>\n' +
+            '<meta charset="utf-8">\n' +
+            '<title>Error</title>\n' +
+            '</head>\n' +
+            '<body>\n' +
+            '<pre>Cannot GET /cart/hello</pre>\n' +
+            '</body>\n' +
+            '</html>\n');
+            done();
         });
     });
 });
