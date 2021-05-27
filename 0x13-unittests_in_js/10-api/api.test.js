e94d288c7d2f70273adbdb1f06afcb9246753e46
zacwoll <zacwoll@gmail.com>
2021-05-26 21:36:40 -0700

    adjusted the tests

--- 0x13-unittests_in_js/10-api/api.test.js
+++ 0x13-unittests_in_js/10-api/api.test.js
@@ -2,74 +2,65 @@ const { expect } = require("chai");
 const request = require('request')
 
 describe('Index page', () => {
-	describe('GET /', () => {
-		it('checks output of GET /', (done) => {
-			request('http://localhost:7865/', (error, response, body) => {
-				expect(response.statusCode).to.equal(200)
-				expect(body).to.equal('Welcome to the payment system')
-			}, done())
-		})
+    it('GET / exists and outputs', (done) => {
+        request('http://localhost:7865/', (error, response, body) => {
+            expect(response.statusCode).to.equal(200)
+            expect(body).to.equal('Welcome to the payment system')
+        }, done())
+    })
+
+	it('GET /cart/:id', (done) => {
+		request('http://localhost:7865/cart/12', (error, response, body) => {
+			expect(response.statusCode).to.equal(200)
+			expect(body).to.equal('Payment methods for cart 12')
+		}, done())
 	})
 
-	describe('GET /cart/:id', () => {
-		it('checks output of curling server for index page with valid cart number', (done) => {
-			request('http://localhost:7865/cart/12', (error, response, body) => {
-				expect(response.statusCode).to.equal(200)
-				expect(body).to.equal('Payment methods for cart 12')
-			}, done())
-		})
-	})
-
-	describe('GET /cart/hello', () => {
-		it('checks output of curling server for index page with invalid cart number', (done) => {
-			request('http://localhost:7865/cart/hello', (error, response, body) => {
-				if (response) {
-					expect(response.statusCode).to.equal(404)
-                    expect(body).to.equal('<!DOCTYPE html>\n' +
-                    '<html lang="en">\n' +
-                    '<head>\n' +
-                    '<meta charset="utf-8">\n' +
-                    '<title>Error</title>\n' +
-                    '</head>\n' +
-                    '<body>\n' +
-                    '<pre>Cannot GET /cart/hello</pre>\n' +
-                    '</body>\n' +
-                    '</html>\n');
-				}
-			}, done())
-		})
-	})
-})
-
-describe('Login page', () => {
-	describe('GET /login', () => {
-		it('checks output of curling login page with userName data', (done) => {
-			const options = {
-				url: 'http://localhost:7865/login',
-				method: 'POST',
-				json: { 'userName': 'Betty' }
+	it('GET /cart/hello', (done) => {
+        request('http://localhost:7865/cart/hello', (error, response, body) => {
+            if (response) {
+                expect(response.statusCode).to.equal(404)
+                expect(body).to.equal('<!DOCTYPE html>\n' +
+                '<html lang="en">\n' +
+                '<head>\n' +
+                '<meta charset="utf-8">\n' +
+                '<title>Error</title>\n' +
+                '</head>\n' +
+                '<body>\n' +
+                '<pre>Cannot GET /cart/hello</pre>\n' +
+                '</body>\n' +
+                '</html>\n');
 			}
-			request(options, (error, response, body) => {
-				expect(response.statusCode).to.equal(200)
-				expect(body).to.equal('Welcome Betty')
-			}, done())
-		})
+		}, done())
 	})
 })
 
 
-describe('Available payments page', () => {
-	describe('GET /available_payments', () => {
-		it('checks output of curling available_payments page', (done) => {
-			request('http://localhost:7865/available_payments', (error, response, body) => {
-				expect(response.statusCode).to.equal(200)
-				expect(body).to.deep.equal(JSON.stringify({
-					payment_methods: {
-						credit_cards: true,
-						paypal: false
-					}
-				}))
-			}, done())
-		})
-	})
+describe('GET /login', () => {
+    it('checks output of curling login page with userName data', (done) => {
+        const options = {
+            url: 'http://localhost:7865/login',
+            method: 'POST',
+            json: { 'userName': 'Betty' }
+        }
+        request(options, (error, response, body) => {
+            expect(response.statusCode).to.equal(200)
+            expect(body).to.equal('Welcome Betty')
+        }, done())
+    })
+})
+
+
+describe('GET /available_payments', () => {
+    it('checks output of curling available_payments page', (done) => {
+        request('http://localhost:7865/available_payments', (error, response, body) => {
+            expect(response.statusCode).to.equal(200)
+            expect(body).to.deep.equal(JSON.stringify({
+                payment_methods: {
+                    credit_cards: true,
+                    paypal: false
+                }
+            }))
+        }, done())
+    })
 })
