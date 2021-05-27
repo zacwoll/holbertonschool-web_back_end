e1049a683eab4cadd0c1c6ac7090982cb790350e
zacwoll <zacwoll@gmail.com>
2021-05-26 19:07:08 -0700

    Changed the export format to fit the checker

--- 0x13-unittests_in_js/0-calcul.js
+++ 0x13-unittests_in_js/0-calcul.js
@@ -1,3 +1,3 @@
-export default function calculateNumber(a, b) {
+module.exports = function calculateNumber(a, b) {
   return Math.round(a) + Math.round(b);
-}
+};
--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,26 +1,25 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul').default;
+const calculateNumber = require('./0-calcul.js');
 
-describe('#calculateNumber()', () => {
-  it('should return 4 when adding 1 and 3', () => {
-    assert.equal(calculateNumber(1, 3), 4);
+describe('calculateNumber', () => {
+  it('round the first argument', () => {
+    assert.equal(calculateNumber(1.0, 0), 1);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(1.7, 0), 2);
   });
-  it('should return 5 when adding 1 and 3.7', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
+
+  it('round the second argument', () => {
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
   });
-  it('should return 5 when adding 1.2 and 3.7', () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-  });
-  it('should return 6 when adding 1.5 and 3.7', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
-  it('should return 0 when adding 0.1 and 0.3', () => {
-    assert.equal(calculateNumber(0.1, 0.3), 0);
-  });
-  it('should return 0 when adding -0.7 and 0.7', () => {
-    assert.equal(calculateNumber(-0.7, 0.7), 0);
-  });
-  it('should return -2 when adding -0.8 and -0.7', () => {
-    assert.equal(calculateNumber(-0.8, -0.7), -2);
+
+  it('should return the right number', () => {
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
+    assert.equal(calculateNumber(1.3, 1.8), 3);
+    assert.equal(calculateNumber(1.6, 1.8), 4);
   });
 });
